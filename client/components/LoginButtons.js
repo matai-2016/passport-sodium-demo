ab921d95e7f969cf7dd278f06fffea53dab99b26
EDA <eda@mac-mini-2.local>
2017-04-10 16:28:02 +1200

    styled order list

--- client/components/LoginButtons.js
+++ client/components/LoginButtons.js
@@ -10,33 +10,35 @@ class LoginButtons extends React.Component {
   render () {
     const authenticated = this.props.authenticated
     return (
-      <div className='container'>
-        {authenticated &&
-          <div className='header clearfix'>
-            <nav>
-              <ul className='nav nav-pills pull-right'>
-                <li role='presentation'>
-                  <Link to='#' onClick={() => this.props.logoutUser()}>Logout</Link>
-                </li>
-              </ul>
-            </nav>
-            <h5 className='text-muted'>Hello {this.props.name}</h5>
-          </div>
-        }
-        {!authenticated &&
-          <div className='header clearfix'>
-            <nav>
-              <ul className='nav nav-pills pull-right'>
-                <li role='presentation'>
-                  <Link to='/login'>Login</Link>
-                </li>
-                <li role='presentation'>
-                  <Link to='/register'>Register</Link>
-                </li>
-              </ul>
-            </nav>
-          </div>
-        }
+      <div className='login-buttons'>
+        <div className='container'>
+          {authenticated &&
+            <div className='header clearfix'>
+              <nav>
+                <ul className='nav nav-pills pull-right'>
+                  <li role='presentation'>
+                    <Link to='#' onClick={() => this.props.logoutUser()}>Logout</Link>
+                  </li>
+                </ul>
+              </nav>
+              <h5 className='text-muted'>Hello {this.props.name}</h5>
+            </div>
+          }
+          {!authenticated &&
+            <div className='header clearfix'>
+              <nav>
+                <ul className='nav nav-pills pull-right'>
+                  <li role='presentation'>
+                    <Link to='/login'>Login</Link>
+                  </li>
+                  <li role='presentation'>
+                    <Link to='/register'>Register</Link>
+                  </li>
+                </ul>
+              </nav>
+            </div>
+          }
+        </div>
       </div>
     )
   }
